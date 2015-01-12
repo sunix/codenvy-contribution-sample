dfb658a3ef0bf01d5f918c0717d0c2186970d100
Sun “suntan@Serli” Tan <sun.tan@serli.com>
2015-01-12 15:33:41 +0200

    Je sis charlie

--- src/main/java/com/codenvy/example/java/App.java
+++ src/main/java/com/codenvy/example/java/App.java
@@ -5,6 +5,6 @@ package com.codenvy.example.java;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Je suis Charlie!");
     }
 }
